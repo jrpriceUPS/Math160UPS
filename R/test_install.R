aa70539dec419e98b1ce9e064b11d805259c4217
Jake Price <jrprice@pugetsound.edu>
2019-06-13 15:50:00 -0700

    added hello world type message to confirm install

---
+++ R/test_install.R
@@ -0,0 +1,15 @@
+#' Test That Your Installation Was Successful
+#'
+#' Prints a welcome banner to indicate that the installation proceeded successfully
+#' @export
+#' @examples
+#' > test_install()
+
+test_install <- function(){
+cat("*********************************************\n*********************************************\n")
+  cat("****                                     ****\n")
+  cat("****     Welcome to Math 160 at UPS!     ****\n")
+  cat("****                                     ****\n")
+  cat("*********************************************\n*********************************************\n\n\n")
+ cat("You have successfully installed a library\nof code that will help you with your\nstatistical analyses in Math 160.") 
+}
