4b850bbdce49427b82102085be7fe96760d38bd0
Jake <jrprice@pugetsound.edu>
2020-07-01 11:15:52 -0700

    Added unfair dice rolling function
    For use in project assuming remote instruction (if not remote, use some sort of weighted dice)

---
+++ R/unfairDie.R
@@ -0,0 +1,14 @@
+unfairDie <- function(rolls){
+  p = c(1,3,2,5,2,4,1)
+  p = p/sum(p)
+  p = cumsum(p)
+  
+  r = runif(rolls)
+  result = rep(0,rolls)
+  
+  for(i in seq(1,rolls)){
+  result[i] = which(r[i]<p)[1]
+  }
+  
+  result
+}
