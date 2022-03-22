f09ae09861c5ae6f7445d3bc4f98a1bb5e1bdcb9
Jake <jrprice@pugetsound.edu>
2022-03-22 15:06:51 -0700

    finally adding malariaLoop in class demo correctly

--- R/malariaLoop.R
+++ R/malariaLoop.R
@@ -1,13 +1,16 @@
-#' Update Code to Latest Version
+#' Run a randomization simulation of the malaria activity in the text
 #'
-#' This function updates your library to the newest version and sets those to become available.
+#' This function runs a randomization simulation to show the 
+#' likelihood of different scenarios under the null hypothesis
+#' for the malaria example from the text. It produces a bar plot
+#' and an assessment of the frequency of the observed result.
 #' @export
-#' > marlariaLoop()
-
+#' @examples
+#' > malariaLoop(reps = 10000)
+malariaLoop <- function(reps = 10000){
 data(malaria)
 
 obs = 5/14 - 6/6
-reps = 10000
 pDiff = rep(0,reps)
 
 for(i in seq(1,reps)){
@@ -18,5 +21,6 @@ pDiff[i] = x[2] - x[1]
 result = table(pDiff)/reps
 rownames(result) = round(sort(unique(pDiff)),2)
 
-barplot(result/reps)
+barplot(result/reps,col = c("skyblue",rep("gray",6)))
 print(paste("In",toString(reps),"replications, we got our observed result with frequency", toString(mean(pDiff == obs))))
+}
