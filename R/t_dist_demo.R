0cb8151fb7d46ea4df01ca384b065dc096f6cdbf
Jake <jrprice@pugetsound.edu>
2023-04-24 12:27:12 -0700

    Updated t dist demo to not use sleep anymore because that's not in the dataframe in spring 2023

--- R/t_dist_demo.R
+++ R/t_dist_demo.R
@@ -3,9 +3,9 @@
 #' This function allows me to run a demo to illustrate the need for a non-normal t-distribution for small samples.
 #' @export
 
-t_dist_demo = function(n = 4,samples = 10000){
+t_dist_demo = function(n = 6,samples = 10000){
   students = get("students")
-  sleep = students$Sleep[!is.na(students$Sleep)]
+  sleep = students$Study[!is.na(students$Study)]
 
   trueMean = mean(sleep)
   trueSD = sd(sleep)/sqrt(n)
@@ -15,10 +15,8 @@ t_dist_demo = function(n = 4,samples = 10000){
     mySample = sample(sleep,n, replace = TRUE)
     ts[i] = (mean(mySample) - trueMean)/(sd(mySample)/sqrt(n))
   }
-
-  hist(ts,freq = FALSE, breaks = 20, main = "Distribution of t-statistics")
-
   xgrid = seq(from = -5,to = 5,length.out = 1000)
+  hist(ts,freq = FALSE, breaks = 20, main = "Distribution of t-statistics",ylim = c(0,max(c(dnorm(xgrid,0,1),dt(xgrid, df = n - 1)))))
 
 
   lines(xgrid,dnorm(xgrid,0,1))
